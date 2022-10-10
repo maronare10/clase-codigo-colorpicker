77e85d1b2ec94937680032eab3ac2ce6af721aed
Mariana Navarro <maronare10@gmail.com>
2022-10-10 15:53:01 -0500

    Clase demo haciendo un color picker usando vitejs + reactjs

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [ backgroundColor, setBackgroundColor ] = useState('')
+
+  const handleChange = (event) => {
+    setBackgroundColor(event.target.value)
+  }
+
+  return (
+    <div className="app" style={{ backgroundColor }}>
+      <span>Selecciona un color:</span>
+      <input
+        type="text"
+        placeholder="#FF0000"
+        onChange={handleChange}
+        value={backgroundColor} />
+    </div>
+  )
+}
+
+export default App
